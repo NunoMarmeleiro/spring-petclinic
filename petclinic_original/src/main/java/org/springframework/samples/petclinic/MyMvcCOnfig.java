d56b88278f0ac3dee7351b6b9833628095829653
KirinDev <111713739+KirinDev@users.noreply.github.com>
2024-05-14 15:32:56 +0100

    included original petclinic monolith version on repo

---
+++ petclinic_original/src/main/java/org/springframework/samples/petclinic/MyMvcCOnfig.java
@@ -0,0 +1,14 @@
+package org.springframework.samples.petclinic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+public class MyMvcCOnfig {
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
+}
