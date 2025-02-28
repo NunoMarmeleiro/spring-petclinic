2d2bd601eb4b9cb2a90a34accaa3332d1fb32cdb
Nuno Marmeleiro <1190922@isep.ipp.pt>
2025-02-28 22:04:10 +0000

    Refactor Process to make the same architecture as the modular monolithic

--- petclinic_microservices/spring-petclinic-owners-service/src/main/java/org/springframework/samples/petclinic/owners/web/mapper/Mapper.java
+++
@@ -1,5 +0,0 @@
-package org.springframework.samples.petclinic.owners.web.mapper;
-
-public interface Mapper<R, E> {
-    E map(E response, R request);
-}
--- petclinic_microservices/spring-petclinic-pets-service/src/main/java/org/springframework/samples/petclinic/pets/web/mapper/Mapper.java
+++
@@ -1,5 +0,0 @@
-package org.springframework.samples.petclinic.pets.web.mapper;
-
-public interface Mapper<R, E> {
-    E map(E response, R request);
-}
