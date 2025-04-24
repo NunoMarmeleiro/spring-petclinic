6fcd0680b9d1a377e9d6c554677b53580deb20e0
Nuno Marmeleiro <1190922@isep.ipp.pt>
2025-04-24 21:50:30 +0100

    Refactor name again

--- petclinic_monolith/src/main/java/org/springframework/samples/petclinic/MyMvcConfig.java
+++ petclinic_monolith/src/main/java/org/springframework/samples/petclinic/MyMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @Configuration
-public class MyMvcConfigs {
+public class MyMvcConfig {
 
 	@Bean
 	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
